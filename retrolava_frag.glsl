633d3f3853429c800395ee96db05c56fe6ee3592
JuPasta <capt.luje@gmail.com>
2023-11-05 14:33:32 +0100

    shader debug

--- retrolava_frag.glsl
+++ retrolava_frag.glsl
@@ -1,9 +1,22 @@
-// Wave pattern shader
+// Simple noise shader
 
 precision mediump float;
 
 uniform vec2 uResolution;
 uniform float uTime;
+
+// Simple hash function
+float hash(float n) { return fract(sin(n) * 1e4); }
+
+// Simple noise function
+float noise(vec2 x) {
+    vec2 p = floor(x);
+    vec2 f = fract(x);
+    f = f*f*(3.0-2.0*f);
+    float n = p.x + p.y*57.0;
+    return mix(mix( hash(n+  0.0), hash(n+  1.0),f.x),
+               mix( hash(n+ 57.0), hash(n+ 58.0),f.x),f.y);
+}
 
 void main() {
     // Normalized pixel coordinates (from 0 to 1)
@@ -12,11 +25,14 @@ void main() {
     // Time varying parameter
     float time = uTime * 0.5;
 
-    // Create a wave pattern using sin function
-    float wave = sin(uv.x * 10.0 + time) * 0.5 + 0.5;
+    // Create a gradient background
+    vec3 bg = mix(vec3(0.1, 0.0, 0.5), vec3(0.5, 0.0, 0.1), uv.y + 0.5 * sin(time));
 
-    // Apply the wave pattern to the gradient
-    vec3 color = mix(vec3(1.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0), wave);
+    // Overlay noise on the gradient
+    float n = noise(uv * 10.0 + time);
+
+    // Mix the gradient with the noise based on the noise value
+    vec3 color = mix(bg, vec3(n), 0.5);
 
     // Output to screen
     gl_FragColor = vec4(color, 1.0);
